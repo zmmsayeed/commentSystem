8225dcb506cfe85230da7a8cef3047beeeb2392e
Zimam Sayeed <zmmsayeed@gmail.com>
2019-12-14 22:31:49 +0530

    Made Comment Card in Dashboard Screen

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import './Dashboard.css'
 
-class Dahsboard extends Component {
+class Dashboard extends Component {
     render() {
         return (
             <div className="container mt-3">
@@ -23,10 +23,66 @@ class Dahsboard extends Component {
                 {/* Post Box Code */}
 
                 {/* Comment Section Code */}
+                <div class="row mt-3">
+                    <div class="col-md-12 col-12">
+                        <div class="comment-box-wrapper">
+                            <div class="comment-box">
+                                <img src="https://previews.123rf.com/images/malydesigner/malydesigner1410/malydesigner141000056/32278325-grunge-gray-background-texture.jpg" class="commenter-image" alt="commenter_image" />
+                                <div class="comment-content">
+                                    <div class="commenter-head"><span class="commenter-name"><a href="" >zmmsayeed@gmail.com</a></span> <span class="comment-date"><i class="far fa-clock"></i>2 days ago</span></div>
+                                    <div class="comment-body">
+                                        <span class="comment">This is comment content Here is nice comment And you are beautiful Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid debitis veritatis, eveniet deserunt eligendi asperiores. Architecto odit voluptatem, quos beatae voluptates quae voluptate enim provident commodi eaque quaerat aut quo!</span>
+                                    </div>
+                                    <div class="comment-footer">
+                                        <span class="comment-likes"><a href="" class="comment-action active"> <i class="fas fa-reply"></i> Reply</a></span> <span class="comment-reply"><a href="" class="comment-action"><i class="fas fa-edit"></i> Edit </a></span>
+                                    </div>
 
+
+                                </div>
+                            </div>
+                            <div class="nested-comments">
+
+                                <div class="comment-box-wrapper">
+                                    <div class="comment-box">
+                                        <img src="https://previews.123rf.com/images/malydesigner/malydesigner1410/malydesigner141000056/32278325-grunge-gray-background-texture.jpg" class="commenter-image" alt="commenter_image" />
+                                        <div class="comment-content">
+                                            <div class="commenter-head"><span class="commenter-name"><a href="" >rafazameri@gmail.com</a></span> <span class="comment-date"><i class="far fa-clock"></i>2 days ago</span></div>
+                                            <div class="comment-body">
+                                                <span class="comment">This is comment content Here is nice comment</span>
+                                            </div>
+                                            <div class="comment-footer">
+                                                <span class="comment-likes"><a href="" class="comment-action active"> <i class="fas fa-reply"></i> Reply</a></span> <span class="comment-reply"><a href="" class="comment-action"><i class="fas fa-edit"></i> Edit </a></span>
+                                            </div>
+
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="comment-box-wrapper">
+                            <div class="comment-box">
+                                <img src="https://previews.123rf.com/images/malydesigner/malydesigner1410/malydesigner141000056/32278325-grunge-gray-background-texture.jpg" class="commenter-image" alt="commenter_image" />
+                                <div class="comment-content">
+                                    <div class="commenter-head"><span class="commenter-name"><a href="" >iamvrl@gmail.com</a></span> <span class="comment-date"><i class="far fa-clock"></i>2 days ago</span></div>
+                                    <div class="comment-body">
+                                        <span class="comment">This is comment content Here is nice comment</span>
+                                    </div>
+                                    <div class="comment-footer">
+                                        <span class="comment-likes"><a href="" class="comment-action active"> <i class="fas fa-reply"></i> Reply</a></span> <span class="comment-reply"><a href="" class="comment-action"><i class="fas fa-edit"></i> Edit </a></span>
+                                    </div>
+
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default Dahsboard;
+export default Dashboard;
