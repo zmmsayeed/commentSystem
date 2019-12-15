862784e5651728919ac63131e672842a5a659aed
Zimam Sayeed <zmmsayeed@gmail.com>
2019-12-15 23:57:23 +0530

    Removed mongodb url for securoty

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "mongodbUrl": "mongodb+srv://mohammad:HCp4IjHJbCCJrsBx@commentproject-vt9th.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodbUrl": "",
     "dbName": "commentProject"
 }
