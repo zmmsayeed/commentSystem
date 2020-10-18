9b929034ee669fc86564ddf5bfa4e757bcbc45e8
Zimam Sayeed <zmmsayeed@gmail.com>
2020-10-18 16:38:34 +0530

    Removed validation message which was constantly showing

--- client/src/components/Dashboard/validation.js
+++ client/src/components/Dashboard/validation.js
@@ -2,7 +2,7 @@ export default function validate(values) {
     const errors = {};
 
     if (!values.comment) {
-        errors.comment = "Comment is Required"
+        errors.comment = " "
     }
 
     return errors
